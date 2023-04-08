2c2aebf848f8aa17cf8a8771f7c881aac70fd6c2
Paul Pham <paul@aquaron.com>
2023-04-08 16:05:54 -0700

    style(rounded): Larger title font, gradation overlay image

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,28 @@
-function playGif(e) {
+function playGif(overlay) {
+  let e = overlay.nextElementSibling;
+
+  if (overlay.dataset.gif) {
+    e = overlay;
+  }
+
   if (!e.dataset.gif) return ;
+
   const bak = e.src;
   e.src = e.dataset.gif;
   e.dataset.gif = bak;
+
+  if (e.dataset.playing) {
+    e.removeAttribute('data-playing');
+    if (e !== overlay)
+      overlay.style.opacity = 1;
+  } else {
+    e.dataset.playing = true;
+    if (e !== overlay)
+      overlay.style.opacity = 0;
+  }
+
+  e.parentElement
+    .nextElementSibling
+    .firstElementChild
+    .style.opacity = e.dataset.playing ? 0.2 : 1;
 }
